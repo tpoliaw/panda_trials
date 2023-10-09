5624dae183c4bd20c9208b277b5b822d98f18f69
Peter Holloway <peter.holloway@diamond.ac.uk>
2023-10-09 11:38:42 +0100

    Reformat tetramm and linkam modules

--- linkam.py
+++ linkam.py
@@ -1,11 +1,16 @@
 from ophyd.v2.core import Device
 from ophyd.v2.epics import epics_signal_rw, epics_signal_r
 
+
 class Linkam(Device):
     def __init__(self, base_pv: str):
-        self.set_point = epics_signal_rw(float, base_pv + ":SETPOINT", base_pv + ":SETPOINT:SET")
+        self.set_point = epics_signal_rw(
+            float, base_pv + ":SETPOINT", base_pv + ":SETPOINT:SET"
+        )
         self.temp = epics_signal_r(float, base_pv + ":TEMP")
-        self.ramp_rate = epics_signal_rw(float, base_pv + ":RAMPRATE", base_pv + ":RAMPRATE:SET")
+        self.ramp_rate = epics_signal_rw(
+            float, base_pv + ":RAMPRATE", base_pv + ":RAMPRATE:SET"
+        )
         self.ramp_time = epics_signal_r(float, base_pv + ":RAMPTIME")
         self.start_heat = epics_signal_rw(float, base_pv + ":STARTHEAT")
         self.dsc = epics_signal_r(bool, base_pv + ":DSC")
--- tetramm.py
+++ tetramm.py
@@ -2,9 +2,11 @@ from enum import IntEnum
 from ophyd.v2.core import Device, wait_for_value
 from ophyd.v2.epics import epics_signal_r, epics_signal_rw
 
+
 class TetrammRange(IntEnum):
     uA = 0
     nA = 1
+
 
 class TetrammTrigger(IntEnum):
     FreeRun = 0
@@ -12,20 +14,24 @@ class TetrammTrigger(IntEnum):
     ExtBulb = 2
     ExtGate = 3
 
+
 class TetrammChannels(IntEnum):
     One = 0
     Two = 1
     Four = 2
 
+
 class TetrammResolution(IntEnum):
     SixteenBits = 0
     TwentyFourBits = 1
+
     def __repr__(self):
         match self:
             case TetrammResolution.SixteenBits:
-                return '16bits'
+                return "16bits"
             case TetrammResolution.TwentyFourBits:
-                return '24bits'
+                return "24bits"
+
 
 class TetrammGeometry(IntEnum):
     Diamond = 0
@@ -60,8 +66,12 @@ class Tetramm(Device):
         self.range = epics_signal_rw(TetrammRange, base_pv + ":DRV:Range")
         self.sample_time = epics_signal_r(float, base_pv + ":DRV:SampleTime_RBV")
 
-        self.values_per_reading = epics_signal_rw(int, base_pv + ":DRV:ValuesPerRead", base_pv + ":DRV:ValuesPerRead_RBV")
-        self.averaging_time = epics_signal_rw(float, base_pv + ":DRV:AveragingTime", base_pv + ":DRV:AveragingTime_RBV")
+        self.values_per_reading = epics_signal_rw(
+            int, base_pv + ":DRV:ValuesPerRead", base_pv + ":DRV:ValuesPerRead_RBV"
+        )
+        self.averaging_time = epics_signal_rw(
+            float, base_pv + ":DRV:AveragingTime", base_pv + ":DRV:AveragingTime_RBV"
+        )
         self.to_average = epics_signal_r(int, base_pv + ":DRV:NumAverage_RBV")
         self.averaged = epics_signal_r(int, base_pv + ":DRV:NumAveraged_RBV")
 
@@ -70,10 +80,14 @@ class Tetramm(Device):
         self.overflows = epics_signal_r(int, base_pv + ":DRV:RingOverflows")
 
         self.channels = epics_signal_rw(TetrammChannels, base_pv + ":DRV:NumChannels")
-        self.resolution = epics_signal_rw(TetrammResolution, base_pv + ":DRV:Resolution")
+        self.resolution = epics_signal_rw(
+            TetrammResolution, base_pv + ":DRV:Resolution"
+        )
         self.trigger = epics_signal_rw(TetrammTrigger, base_pv + ":DRV:TriggerMode")
         self.bias = epics_signal_rw(bool, base_pv + ":DRV:BiasState")
-        self.bias_volts = epics_signal_rw(float, base_pv + ":DRV:BiasVoltage", base_pv + ":DRV:BiasVoltage_RBV")
+        self.bias_volts = epics_signal_rw(
+            float, base_pv + ":DRV:BiasVoltage", base_pv + ":DRV:BiasVoltage_RBV"
+        )
 
         self.geometry = epics_signal_rw(TetrammGeometry, base_pv + ":DRV:Geometry")
 
