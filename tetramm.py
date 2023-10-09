2d6f023bd973a0b642bc0e2520cf295ca55c03d6
Peter Holloway <peter.holloway@diamond.ac.uk>
2023-10-09 11:38:22 +0100

    Add first pass at handling tetramm collection times

--- tetramm.py
+++ tetramm.py
@@ -1,5 +1,5 @@
 from enum import IntEnum
-from ophyd.v2.core import Device
+from ophyd.v2.core import Device, wait_for_value
 from ophyd.v2.epics import epics_signal_r, epics_signal_rw
 
 class TetrammRange(IntEnum):
@@ -27,8 +27,36 @@ class TetrammResolution(IntEnum):
             case TetrammResolution.TwentyFourBits:
                 return '24bits'
 
+class TetrammGeometry(IntEnum):
+    Diamond = 0
+    Square = 1
+
+
 class Tetramm(Device):
-    def __init__(self, base_pv):
+    base_sample_rate: int
+    """base rate"""
+
+    maximum_readings_per_frame: int
+    """An upper limit on the dimension of frames collected during a collection"""
+
+    minimum_values_per_reading: int
+    """A lower bound on the number of values that will be averaged to create a single reading"""
+
+    idle_acquire: bool
+    """Whether the tetramm should be left acquiring after a collection"""
+
+    idle_trigger_state: TetrammTrigger
+    """The state the trigger should be left in when no collection is running"""
+
+    def __init__(
+        self,
+        base_pv,
+        minimum_values_per_reading=5,
+        maximum_readings_per_frame=1_000,
+        base_sample_rate=100_000,
+        idle_acquire=True,
+        idle_trigger_state=TetrammTrigger.FreeRun,
+    ):
         self.range = epics_signal_rw(TetrammRange, base_pv + ":DRV:Range")
         self.sample_time = epics_signal_r(float, base_pv + ":DRV:SampleTime_RBV")
 
@@ -46,3 +74,31 @@ class Tetramm(Device):
         self.trigger = epics_signal_rw(TetrammTrigger, base_pv + ":DRV:TriggerMode")
         self.bias = epics_signal_rw(bool, base_pv + ":DRV:BiasState")
         self.bias_volts = epics_signal_rw(float, base_pv + ":DRV:BiasVoltage", base_pv + ":DRV:BiasVoltage_RBV")
+
+        self.geometry = epics_signal_rw(TetrammGeometry, base_pv + ":DRV:Geometry")
+
+        self.base_sample_rate = base_sample_rate
+        self.maximum_readings_per_frame = maximum_readings_per_frame
+        self.minimum_values_per_reading = minimum_values_per_reading
+
+        self.idle_acquire = idle_acquire
+        self.idle_trigger_state = idle_trigger_state
+
+    async def set_frame_time(self, seconds):
+        await self.averaging_time.set(seconds / 1_000)
+        values_per_reading = (
+            seconds * self.base_sample_rate / self.maximum_readings_per_frame
+        )
+        if values_per_reading < self.minimum_values_per_reading:
+            values_per_reading = self.minimum_values_per_reading
+        await self.values_per_reading.set(values_per_reading)
+        await self._refresh_file_size_dimensions(seconds)
+
+    async def _refresh_file_size_dimensions(self, seconds):
+        if not self.idle_acquire or self.idle_trigger_state != TetrammTrigger.FreeRun:
+            self.acquire.set(False)
+            self.trigger.set(TetrammTrigger.FreeRun)
+            self.acquire.set(True)
+            target = await self.to_average.get_value()
+            await wait_for_value(self.averaged, target, seconds * 2)
+            self.acquire.set(False)
